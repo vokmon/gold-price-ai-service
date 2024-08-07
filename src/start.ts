135bd9da3e1b369fd53fc50725766e8b84e98af2
Arnon Ruangthanawes <aruangthanawes@go7.io>
2024-08-07 09:23:04 +0700

    Fix start script

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,4 @@
 import MainApplication from "./main-application.ts";
 const mainApp = new MainApplication();
 await mainApp.runProcess();
-await mainApp.monitorPrice();
+await mainApp.monitorPrice(100);
