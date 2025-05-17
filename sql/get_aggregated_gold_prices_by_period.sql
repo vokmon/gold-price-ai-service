d6f44a891028368e5b1ebe00be6253c476785bf1
Arnon Ruangthanawes <arnon.ruangthanawes@screencloud.io>
2025-05-17 18:21:23 +0700

    Fix - Set a fixed search_path

--- sql/get_aggregated_gold_prices_by_period.sql
+++ sql/get_aggregated_gold_prices_by_period.sql
@@ -1,23 +1,24 @@
-create or replace function get_aggregated_gold_prices_by_period(
+CREATE OR REPLACE FUNCTION public.get_aggregated_gold_prices_by_period(
   period text,
   start_ts timestamptz,
   end_ts timestamptz
 )
-returns table (
+RETURNS TABLE (
   date_time timestamptz,
   min_sell bigint,
   max_sell bigint
 )
-as $$
-begin
-  return query
-  select
-    date_trunc(period, created_time) as date_time,
-    min(sell) as min_sell,
-    max(sell) as max_sell
-  from goldprice
-  where created_time between start_ts and end_ts
-  group by date_time
-  order by date_time;
-end;
-$$ language plpgsql;
+SET search_path = public
+AS $$
+BEGIN
+  RETURN QUERY
+  SELECT
+    date_trunc(period, created_time) AS date_time,
+    MIN(sell) AS min_sell,
+    MAX(sell) AS max_sell
+  FROM goldprice
+  WHERE created_time BETWEEN start_ts AND end_ts
+  GROUP BY date_time
+  ORDER BY date_time;
+END;
+$$ LANGUAGE plpgsql;
